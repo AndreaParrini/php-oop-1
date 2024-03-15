91fa260ab68095691d105e84512c83ca26a0388d
AndreaParrini <andrea.parrini26@gmail.com>
2024-03-15 19:26:17 +0100

    add property static type
    add property poster
    
    add methods to set duration
    
    add methods to get type

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 class Movie
 {
     public static $language = 'Italian';
+    public static $type = 'Movie';
 
     /**
      * @param String $title Name of movie
@@ -10,11 +11,26 @@ class Movie
      * @param Array $genres Genres of movie
      * 
      */
-    public function __construct(public string $title, public int $length, public array $genres)
+    public function __construct(public string $title, public int $length, public array $genres, public string $poster, public string $duration = '')
     {
         $this->title = $title;
         $this->length = $length;
         $this->genres = $genres;
+        $this->poster = $poster;
+    }
+
+    /**
+     * Function to set duration of movie
+     */
+    public function setDuration($length)
+    {
+        if ($length < 120) {
+            $this->duration = 'Breve';
+        } elseif ($length > 180) {
+            $this->duration = 'Lungo';
+        } else {
+            $this->duration = 'Medio';
+        }
     }
 
     /**
@@ -28,36 +44,44 @@ class Movie
     }
 
     /**
-     * Function to get langue of movie
+     * Function to get languege of movie
      */
-    function getLanguageMovie()
+    public static function getLanguageMovie()
     {
         return self::$language;
+    }
+
+    /**
+     * Function to get type
+     */
+    public function getType()
+    {
+        return self::$type;
     }
 };
 
 // crep l'array richiamando la class e passandogli tutti i valori definiti nel costruttore.
 $movies = [
-    new Movie('Avatar 2', 192, ['Sci-fi', 'Action']),
-    new Movie('I Am Legend', 101, ['Action', 'Sci-fi']),
-    new Movie('Dune - Parte Due', 186, ['Sci-fi', 'Adventure']),
-    new Movie('Povere creature!', 141, ['Comedy', 'Sci-fi']),
-    new Movie('Argylle - La super spia', 139, ['Action', 'Comedy']),
-    new Movie('Fast & Furious 10', 141, ['Drama', 'Action']),
-    new Movie('Aquaman e il regno perduto', 115, ['Action', 'Fantasy']),
-    new Movie('Creed III', 113, ['Sport', 'Action']),
-    new Movie('Oppenheimer', 180, ['Thriller', 'Mistery']),
-    new Movie('Deadpool 2', 120, ['Comedy', 'Action']),
-    new Movie('Avengers: Endgame', 182, ['Sci-fi', 'Action']),
-    new Movie('Natale a Miami', 100, ['Comedy', 'Romantic']),
-    new Movie('I soliti idioti 3 - Il ritorno', 90, ['Comedy']),
+    new Movie('Avatar 2', 192, ['Sci-fi', 'Action'], 'https://picsum.photos/200/300'),
+    new Movie('I Am Legend', 101, ['Action', 'Sci-fi'], 'https://picsum.photos/200/300'),
+    new Movie('Dune - Parte Due', 186, ['Sci-fi', 'Adventure'], 'https://picsum.photos/200/300'),
+    new Movie('Povere creature!', 141, ['Comedy', 'Sci-fi'], 'https://picsum.photos/200/300'),
+    new Movie('Argylle - La super spia', 139, ['Action', 'Comedy'], 'https://picsum.photos/200/300'),
+    new Movie('Fast & Furious 10', 141, ['Drama', 'Action'], 'https://picsum.photos/200/300'),
+    new Movie('Aquaman e il regno perduto', 115, ['Action', 'Fantasy'], 'https://picsum.photos/200/300'),
+    new Movie('Creed III', 113, ['Sport', 'Action'], 'https://picsum.photos/200/300'),
+    new Movie('Oppenheimer', 180, ['Thriller', 'Mistery'], 'https://picsum.photos/200/300'),
+    new Movie('Deadpool 2', 120, ['Comedy', 'Action'], 'https://picsum.photos/200/300'),
+    new Movie('Avengers: Endgame', 182, ['Sci-fi', 'Action'], 'https://picsum.photos/200/300'),
+    new Movie('Natale a Miami', 100, ['Comedy', 'Romantic'], 'https://picsum.photos/200/300'),
+    new Movie('I soliti idioti 3 - Il ritorno', 90, ['Comedy'], 'https://picsum.photos/200/300'),
 ];
 
 // ciclo all'interno dell'array per stampare tutti i generi di quel film.
 foreach ($movies as $movie) {
     $movie->getAllGenre();
-    $lang = $movie->getLanguageMovie();
-    var_dump($lang);
+    echo Movie::getLanguageMovie();
+    /* Movie::getPosterOfMovie(); */
 }
 
 var_dump($movies);
