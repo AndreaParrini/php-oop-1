b6a648c121bccc0d6f993acc228f44d88a12ba18
AndreaParrini <andrea.parrini26@gmail.com>
2024-03-15 18:52:51 +0100

    add array movies and creat some movie
    add property static
    create two methods
    Modify constructor

--- index.php
+++ index.php
@@ -2,40 +2,62 @@
 
 class Movie
 {
-    public $title;
-    public $length;
-    public $duration;
-    public $type;
+    public static $language = 'Italian';
 
     /**
      * @param String $title Name of movie
      * @param Number $lenght Time of movie
-     * @param Array $type Type of movie
+     * @param Array $genres Genres of movie
      * 
      */
-    public function __construct($title, $length, $type)
+    public function __construct(public string $title, public int $length, public array $genres)
     {
         $this->title = $title;
         $this->length = $length;
-        $this->type = $type;
+        $this->genres = $genres;
     }
 
-    public function setDuration($length)
+    /**
+     * Function to stamp all genre of movie
+     */
+    public function getAllGenre()
     {
-        if ($length < 120) {
-            $this->duration = 'Breve';
-        } elseif ($length > 300) {
-            $this->duration = 'Lungo';
-        } else {
-            $this->duration = 'Normale';
+        foreach ($this->genres as $genre) {
+            echo $genre;
         }
+    }
+
+    /**
+     * Function to get langue of movie
+     */
+    function getLanguageMovie()
+    {
+        return self::$language;
     }
 };
 
-$avatar = new Movie('Avatar', 200, ['Drama', 'Action']);
-$avatar->setDuration($avatar->length);
-var_dump($avatar);
+// crep l'array richiamando la class e passandogli tutti i valori definiti nel costruttore.
+$movies = [
+    new Movie('Avatar 2', 192, ['Sci-fi', 'Action']),
+    new Movie('I Am Legend', 101, ['Action', 'Sci-fi']),
+    new Movie('Dune - Parte Due', 186, ['Sci-fi', 'Adventure']),
+    new Movie('Povere creature!', 141, ['Comedy', 'Sci-fi']),
+    new Movie('Argylle - La super spia', 139, ['Action', 'Comedy']),
+    new Movie('Fast & Furious 10', 141, ['Drama', 'Action']),
+    new Movie('Aquaman e il regno perduto', 115, ['Action', 'Fantasy']),
+    new Movie('Creed III', 113, ['Sport', 'Action']),
+    new Movie('Oppenheimer', 180, ['Thriller', 'Mistery']),
+    new Movie('Deadpool 2', 120, ['Comedy', 'Action']),
+    new Movie('Avengers: Endgame', 182, ['Sci-fi', 'Action']),
+    new Movie('Natale a Miami', 100, ['Comedy', 'Romantic']),
+    new Movie('I soliti idioti 3 - Il ritorno', 90, ['Comedy']),
+];
 
-$iAmLegend = new Movie('I Am Legend', 100, 'Drama');
-$iAmLegend->setDuration($iAmLegend->length);
-var_dump($iAmLegend);
+// ciclo all'interno dell'array per stampare tutti i generi di quel film.
+foreach ($movies as $movie) {
+    $movie->getAllGenre();
+    $lang = $movie->getLanguageMovie();
+    var_dump($lang);
+}
+
+var_dump($movies);
